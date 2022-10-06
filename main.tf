bceb0a596dcce3b9547b6ae13a90e606db9cd5cd
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-10-06 09:23:27 +0200

    update

--- main.tf
+++ main.tf
@@ -1,18 +1,15 @@
 resource "aws_eks_cluster" "this" {
   name     = var.name
   role_arn = aws_iam_role.this.arn
-
   vpc_config {
     subnet_ids = var.subnet_ids
   }
-
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.this_AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.this_AmazonEKSVPCResourceController,
   ]
-
   tags = {
     Name        = lower(var.name)
     Environment = lower(var.environment)
--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-myproj-euc1-dev"
+    bucket = "terraform-myproje-euc1-dev"
     key    = "eks/terraform.tfstate"
     region = "eu-central-1"
   }
