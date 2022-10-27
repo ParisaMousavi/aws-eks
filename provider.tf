b4e2253fd0383e9e77cc336dddd2903b1097d9f0
Parisa Moosavinezhad <parisam@microsoft.com>
2022-10-27 14:07:01 +0200

    update

--- provider.tf
+++ provider.tf
@@ -1,7 +1,18 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.33.0"
+    }
+  }
   backend "s3" {
     bucket = "terraform-myproje-euc1-dev"
-    key    = "eks/terraform.tfstate"
+    key    = "for-tf-validate/terraform.tfstate"
     region = "eu-central-1"
   }
 }
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
