09fbc7cf6c09165c6bd86bd2ee6b930d036abcb7
Parisa Moosavinezhad <parisam@microsoft.com>
2022-10-27 14:53:50 +0200

    update

--- input.tf
+++ input.tf
@@ -1,12 +1,14 @@
-#-----------------------------------------
-# General variables are in all inputs
-#-----------------------------------------
 variable "name" {
   type = string
 }
 
+# Amazon EKS creates cross-account elastic network interfaces in these subnets to allow communication between your worker nodes and the Kubernetes control plane.
 variable "subnet_ids" {
   type = list(string)
+  validation {
+    condition = length(var.subnet_ids) >= 2
+    error_message = "List of subnet IDs. Must be in at least two different availability zones."
+  }
 }
 
 variable "additional_tags" {
